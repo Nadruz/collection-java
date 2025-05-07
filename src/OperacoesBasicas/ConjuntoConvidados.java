a9ce6670ad04297ab32691eec3ff0b3955aa651f
Nadruz <juliananadruz@gmail.com>
2025-05-07 20:38:50 -0300

    Learning Set Interface

---
+++ src/OperacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,34 @@
+package OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados(Set<Convidado> convidadoSet) {
+        this.convidadoSet = new HashSet<>();
+    }
+    
+    public void adicionarConvidadoSet(String nome, int codigoConvite) {
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+    
+    public void removerConvidadoPorCodigoConvite(int codigoConvite) {
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet) {
+            if(c.getCodigoConvite() == codigoConvite) {
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+    }
+    public int contarConvidados() {
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados() {
+        System.out.println(convidadoSet);
+    }
+}
