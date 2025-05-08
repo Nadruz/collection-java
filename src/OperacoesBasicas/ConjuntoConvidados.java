407a5a05a1190a8c23786cb845b6153ec6db6d5d
Nadruz <juliananadruz@gmail.com>
2025-05-08 19:28:55 -0300

    Learning Set Interface

--- src/OperacoesBasicas/ConjuntoConvidados.java
+++ src/OperacoesBasicas/ConjuntoConvidados.java
@@ -24,11 +24,28 @@ public class ConjuntoConvidados {
         }
         convidadoSet.remove(convidadoParaRemover);
     }
+
     public int contarConvidados() {
         return convidadoSet.size();
+    }
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
     }
 
     public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
-}
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        
+        conjuntoConvidados.adicionarConvidadoSet("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidadoSet("Convidado 2", 1233);
+        conjuntoConvidados.adicionarConvidadoSet("Convidado 3", 1234);
+
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de convidados");
+
+    }
+        
+    }
--- src/OperacoesBasicas/Convidado.java
+++ src/OperacoesBasicas/Convidado.java
@@ -3,15 +3,40 @@ package OperacoesBasicas;
 public class Convidado {
     private String nome;
     private int codigoConvite;
+    
     public Convidado(String nome, int codigoConvite) {
         this.nome = nome;
         this.codigoConvite = codigoConvite;
     }
+
     public String getNome() {
         return nome;
     }
+
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigoConvite;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (getClass() != o.getClass())
+            return false;
+        Convidado other = (Convidado) o;
+        if (codigoConvite != other.codigoConvite)
+            return false;
+        return true;
     }
 
     
